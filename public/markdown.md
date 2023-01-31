bacc1cb43de6a7a5e239dfa53301c3bbbf556313
kane1997 <648636410@qq.com>
2023-01-31 16:38:51 +1100

    first commit

---
+++ public/markdown.md
@@ -0,0 +1,15 @@
+# Introduction
+
+This is a sample Markdown file.
+
+## Section 1
+
+This is the first section.
+
+## Section 2
+
+This is the second section.
+
+## Conclusion
+
+That's it!
--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { marked } from "marked";
+import smoothscroll from "smoothscroll-polyfill";
 
-function App() {
+
+const App = () => {
+  const [markdown, setMarkdown] = useState("");
+  const [tableOfContents, setTableOfContents] = useState([]);
+
+  useEffect(() => {
+    // Fetch the Markdown text and parse it into HTML
+    fetch("/markdown.md")
+      .then(res => res.text())
+      .then(text => {
+        console.log({ text })
+        setMarkdown(marked(text));
+        extractTableOfContents(text);
+      });
+  }, []);
+
+  const extractTableOfContents = text => {
+    // Extract the headings from the Markdown text
+    const lines = text.split("\n");
+    const headings = [];
+    lines.forEach(line => {
+      if (line.startsWith("#")) {
+        headings.push(line);
+      }
+    });
+
+    // Generate the table of contents
+    const toc = headings.map(heading => {
+      const level = heading.split(" ")[0].length;
+      const text = heading.substring(level + 1);
+      const id = text.toLowerCase().replace(/ /g, "-");
+      return { level, text, id };
+    });
+
+    setTableOfContents(toc);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="markdown-display">
+      <aside className="table-of-contents">
+        {tableOfContents.map(item => (
+          <a
+            key={item.id}
+            href={`#${item.id}`}
+            onClick={() => {
+              smoothscroll.polyfill();
+              window.scroll({
+                top: document.querySelector(`#${item.id}`).offsetTop,
+                behavior: "smooth"
+              });
+            }}
+          >
+            {item.text}
+          </a>
+        ))}
+      </aside>
+      <div
+        className="markdown-content"
+        dangerouslySetInnerHTML={{ __html: markdown }}
+      />
     </div>
   );
-}
+};
 
 export default App;
